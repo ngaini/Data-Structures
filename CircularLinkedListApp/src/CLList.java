c4654dde834336b7cc8c3e952a998e678a5d0c7e
Nathan Gaini <nathan.gaini3@gmail.com>
2016-01-27 22:11:50 -0800

    all operations on Circular linked list

--- CircularLinkedListApp/src/CLList.java
+++ CircularLinkedListApp/src/CLList.java
@@ -36,9 +36,105 @@ public class CLList {
 	}
 	
 	//insert at start
+	public void insertAtStart(int data)
+	{
+		Node newLink = new Node(data);
+		Node current = first ;
+		// check if list empty
+		if(first == null)
+		{
+			first = newLink;
+			return;
+		}
+		// traverse untill the last node
+		while(current.next!= first )
+		{
+			current = current.next ;
+			
+		}
+		// inset oprations
+		newLink.next = current.next;
+		current.next = newLink;
+		first =newLink;
+		
+	}
 	
 	//delete at last
+	public void deleteAtEnd()
+	{
+		//create curr and prev for traversal
+		Node current = first;
+		Node previous = first;
+		//check if list empty
+		if(first ==null)
+		{
+			System.out.println("List empty");
+			return;
+		}
+		
+		
+		//check if list has only one node
+		if(first.next == first)
+		{
+			first = null;
+		}
+		
+		//traverse
+		while(current.next!= first)
+		{
+			previous = current;
+			current = current.next;
+		}
+		//deletion operation
+		previous.next = current.next;
+		current.next = null;
+	}
 	
 	//delete at start
-
+	public void deleteatStart()
+	{
+		//node for traversal
+		Node current = first;
+		//temp node
+		Node temp = first;
+		// if list empty
+		if(first ==null)
+		{
+			System.out.println("List empty");
+			return;
+		}
+		
+		// traverse till end
+		while(current.next!=first)
+		{
+			current = current.next;
+		}
+		//delete first node
+		current.next = first.next;
+		first = first.next;
+		temp.next = null;
+	}
+	
+	//display List
+	public void display()
+	{
+		Node current = first ;
+		
+		if(first == null)
+		{
+			System.out.println("List empty , nothing to display");
+			return;
+		}
+		if(first.next == first)
+		{
+			current.displayNode();
+			return;
+		}
+		
+		while(current.next!=first)
+		{
+			current.displayNode();
+		}
+		
+	}
 }
