6fed82507965dbbe8b4dfdfc19b770619f48dcf9
Nathan Gaini <nathan.gaini3@gmail.com>
2016-02-04 02:36:25 -0800

    karumanchi version

---
+++ MergeSortApplication/src/MSApp.java
@@ -0,0 +1,72 @@
+import java.time.chrono.MinguoEra;
+
+
+public class MSApp {
+	
+	// merge method
+	
+	public void MergeSort(int[] A, int[] temp, int left, int right )
+	{
+		int mid;
+		if(right>left)
+		{
+			mid=(right+left)/2;
+			MergeSort(A, temp, left, mid);
+			MergeSort(A, temp, mid+1, right);
+			// now to merge the two arrays
+		}
+	}
+	
+	public void Merge(int[] A, int[] temp, int left,int mid, int right )
+	{
+		int i, left_end, size, temp_pos;
+		left_end = mid -1;
+		temp_pos = left;
+		size = right - left+1;
+		while((left<=left_end)&&(mid<=right))
+		{
+			if(A[left]<= A[mid])
+			{
+				temp[temp_pos]= A[left];
+				temp_pos+=1;
+				left+=1;
+			}
+			else
+			{
+				temp[temp_pos]=A[mid];
+				temp_pos+=1;
+				mid+=1;
+				
+			}
+		}
+		while(left<=left_end)
+		{
+			temp[temp_pos]= A[left];
+			temp_pos+=1;
+			left+=1;
+		}
+		while(mid<=right)
+		{
+			temp[temp_pos]=A[mid];
+			temp_pos+=1;
+			mid+=1;
+		}
+		for(i=0; i<=size; i++)
+		{
+			A[right]= temp[right];
+			right= right-1;
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MSApp m1= new MSApp();
+		int[] A= new int[] {10,15,9,1,8,50,2};
+		int left=0;
+		int right=6;
+		int[] temp = new int[];
+		m1.MergeSort(A, temp, left, right);
+		
+	}
+
+}
